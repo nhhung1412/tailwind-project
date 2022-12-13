2442044f93a8a3193df22b300ac90cbff6d48456
{nguyen huu hung} <{nhhung14121999@gmail.com>
2022-12-13 14:17:58 +0700

    Add-header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'karla': ['Karla', 'sans-serif']
+      },
+      colors: {
+        'light-coffee' : '#C89F94'
+      }
+    },
   },
   plugins: [],
 }
