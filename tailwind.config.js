b0a176e1d7c57b93a1011bf2afdc8103b5e7e365
{nguyen huu hung} <{nhhung14121999@gmail.com>
2022-12-16 09:01:33 +0700

    responsive_header

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,15 @@ module.exports = {
       },
       colors: {
         'light-coffee' : '#C89F94'
+      },
+      keyframes: {
+        slideDown: {
+          '0%':{transform:'translateY(-100%)'},
+          '100%':{transform:'translateY(0)'}
+        },
+      },
+      animation: {
+        slideDown: 'slideDown 0.5s ease-in-out',
       }
     },
   },
