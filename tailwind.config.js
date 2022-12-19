6647a508fd0a4ff53c63444aab5806b74c18751b
unknown <nhhung14121999@gmail.com>
2022-12-19 13:10:53 +0700

    add featured

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,22 +6,32 @@ module.exports = {
         'karla': ['Karla', 'sans-serif']
       },
       colors: {
-        'light-coffee' : '#C89F94'
+        'light-coffee' : '#C89F94',
+        'brown-coffee' : '#A25F4B'
       },
       keyframes: {
         slideDown: {
           '0%':{transform:'translateY(-100%)'},
           '100%':{transform:'translateY(0)'}
         },
+        slideUp: {
+          '0%':{transform:'translate3d(0px,8px,0px)',opacity:0},
+          '100%':{transform:'translate3d(0px,0px,0px)',opacity:1},
+        }
+       
       },
       animation: {
         slideDown: 'slideDown 0.5s ease-in-out',
+        slideUp: 'slideUp .4s ease-in'
+        
       },
       height: {
         'header': '80px',
       },
       backgroundImage: {
-        'slider-bg':'url("./img/slider-header.jpg")'
+        'slider':'url("./img/slider-header.jpg")',
+        'featured-mug1':'url("./img/featured-mug1.jpg")',
+        'featured-mug2':'url("./img/featured-mug2.jpg")',
       }
     },
   },
